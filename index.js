0f45c14d013a15ae841454c5b3f61c5210fec64e
mirazmaliha <mstmaliha172@gmail.com>
2025-11-30 18:33:17 +0600

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require("dotenv").config();
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 app.use(express.json());
